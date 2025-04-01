990e8f48a1425dcebe4d27bd6cf736440aac9540
Umar <umar.dev500@gmail.com>
2025-04-01 07:01:54 +0700

    setup for react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import {Login} from '@/screens/login';
+import {DarkTheme, NavigationContainer} from '@react-navigation/native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import './global.css';
 
 const App = () => {
-  return <Login />;
+  return (
+    <>
+      <GestureHandlerRootView>
+        <NavigationContainer theme={DarkTheme}>
+          <Login />
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </>
+  );
 };
 
 export default App;
