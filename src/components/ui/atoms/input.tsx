d9dec932d030a1f6d7364afacd0df282a7c47454
Umar <umar.dev500@gmail.com>
2025-04-02 16:20:36 +0700

    use container for input

--- src/components/ui/atoms/input.tsx
+++ src/components/ui/atoms/input.tsx
@@ -1,5 +1,12 @@
-import React, {useState} from 'react';
-import {KeyboardTypeOptions, TextInput, View} from 'react-native';
+import {Icon, IconName} from '@/components/custom-icon';
+import React, {RefObject, useImperativeHandle, useState} from 'react';
+import {
+  KeyboardTypeOptions,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
 const getInputSize = (size?: 'lg' | 'xl'): string => {
   let hClass;
@@ -17,36 +24,129 @@ const getInputSize = (size?: 'lg' | 'xl'): string => {
   return hClass;
 };
 
-type InputProps = {
-  placeholder?: string;
-  leadingIcon?: React.ReactNode;
-  size?: 'lg' | 'xl';
-  keyboardType?: KeyboardTypeOptions;
+// Function to render an icon if it's provided
+const renderIcon = (
+  iconColor: string,
+  iconSize: number,
+  icon?: React.ReactNode,
+  iconName?: IconName,
+  isIconFill?: boolean,
+) => {
+  if (icon) {
+    return icon;
+  }
+  if (iconName) {
+    return (
+      <Icon
+        name={iconName}
+        size={iconSize}
+        color={iconColor}
+        fill={isIconFill}
+      />
+    );
+  }
+  return null;
 };
 
-const Input = ({placeholder, leadingIcon, size, keyboardType}: InputProps) => {
+type InputProps = {
+  value?: string;
+  placeholder?: string;
+  leadingIcon?: React.ReactNode;
+  leadingIconSize?: number;
+  leadingIconName?: IconName;
+  trailingIcon?: React.ReactNode;
+  trailingIconName?: IconName;
+  trailingIconSize?: number;
+  iconColor?: string;
+  isIconFill?: boolean;
+  iconSize?: number;
+  size?: 'lg' | 'xl';
+  keyboardType?: KeyboardTypeOptions;
+  editable?: boolean;
+  onPress?: () => void;
+  ref?: RefObject<InputRef | null>;
+};
+
+// 🔹 Define the type for the InputRef
+export type InputRef = {
+  getValue: () => string;
+  setValue: (text: string) => void;
+  clear: () => void;
+};
+
+const Input = ({
+  value,
+  placeholder,
+  leadingIcon,
+  leadingIconName,
+  leadingIconSize = 24,
+  trailingIcon,
+  trailingIconName,
+  trailingIconSize = 24,
+  iconColor = 'white',
+  isIconFill = false,
+  iconSize,
+  size,
+  keyboardType,
+  editable = true,
+  onPress,
+  ref,
+}: InputProps) => {
   const [focused, setFocused] = useState(false);
   const inputSizeClass = getInputSize(size);
+  const [inputValue, setInputValue] = useState(value || '');
+
+  useImperativeHandle(ref, () => {
+    return {
+      getValue: () => inputValue,
+      setValue: (text: string) => setInputValue(text),
+      clear: () => setInputValue(''),
+    };
+  });
 
   return (
-    <View
-      className={`flex-row items-center gap-2 border ${
-        !focused ? 'border-white/50' : 'border-white/90'
-      }  px-4 ${inputSizeClass} h- rounded-2xl`}>
-      {leadingIcon && leadingIcon}
-      <TextInput
-        keyboardType={keyboardType}
-        onFocus={() => {
-          setFocused(true);
-        }}
-        onBlur={() => {
-          setFocused(false);
-        }}
-        className="text-base text-white flex-1 h-full"
-        placeholderTextColor={'rgba(255,255,255,0.5)'}
-        placeholder={placeholder}
-      />
-    </View>
+    <Pressable disabled={!onPress} onPress={onPress}>
+      <View
+        className={`flex-row items-center gap-2 border ${
+          !focused ? 'border-white/50' : 'border-white/90'
+        }  px-4 ${inputSizeClass} h- rounded-2xl`}>
+        {renderIcon(
+          iconColor,
+          iconSize ?? leadingIconSize,
+          leadingIcon,
+          leadingIconName,
+          isIconFill,
+        )}
+        <View className="flex-1 flex-row items-center">
+          {inputValue.length < 1 && !value ? (
+            <Text className="absolute text-base text-white/50">
+              {placeholder}
+            </Text>
+          ) : null}
+          <TextInput
+            value={value}
+            keyboardType={keyboardType}
+            onFocus={() => {
+              setFocused(true);
+            }}
+            onBlur={() => {
+              setFocused(false);
+            }}
+            editable={editable}
+            onChangeText={setInputValue}
+            className="text-base px-0 text-white/85 flex-1 h-full"
+            placeholderTextColor={'rgba(255,255,255,0.5)'}
+          />
+        </View>
+        {renderIcon(
+          iconColor,
+          iconSize ?? trailingIconSize,
+          trailingIcon,
+          trailingIconName,
+          isIconFill,
+        )}
+      </View>
+    </Pressable>
   );
 };
 
