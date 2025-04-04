132ef4f2f60e0631db275633b731cdcdd2559ab8
Umar <umar.dev500@gmail.com>
2025-04-04 18:38:52 +0700

    initial for tab bar

--- src/screens/analytics.tsx
+++ src/screens/analytics.tsx
@@ -1,7 +1,8 @@
 import {Spacer} from '@/components/spacer';
-import {StatisticChart, TransactionList} from '@/components/ui/organisms';
+import {TransactionCard} from '@/components/ui/molecules';
+import {StatisticChart} from '@/components/ui/organisms';
 import {dummyTransactions} from '@/data';
-import {Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 export const Analytics = () => {
@@ -13,15 +14,40 @@ export const Analytics = () => {
             <Text className="text-white text-2xl">Statistics</Text>
           </View>
 
-          <View className="py-4">
-            <StatisticChart />
-          </View>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View className="flex-row overflow-hidden bg-white/15 rounded-xl h-10 w-full mt-2 mb-4">
+              <View className="flex-1 bg-white rounded-xl overflow-hidden items-center justify-center">
+                <Text className="text-black text-base font-medium">Weekly</Text>
+              </View>
+              <View className="flex-1 items-center justify-center">
+                <Text className="text-white text-base font-medium">
+                  Monthly
+                </Text>
+              </View>
+              <View className="flex-1 items-center justify-center">
+                <Text className="text-white text-base font-medium">Yearly</Text>
+              </View>
+            </View>
 
-          <View className="mt-6">
-            <Text className="text-white text-xl">Transactions</Text>
-            <Spacer className="h-4" />
-            <TransactionList data={dummyTransactions} />
-          </View>
+            <View className="py-4">
+              <StatisticChart />
+            </View>
+
+            <View className="mt-6">
+              <Text className="text-white text-xl">Transactions</Text>
+              <Spacer className="h-4" />
+              {dummyTransactions.map((transaction, index) => (
+                <View key={transaction.id}>
+                  <TransactionCard
+                    delay={index * 100}
+                    key={transaction.id}
+                    {...transaction}
+                  />
+                  <Spacer className="h-4" />
+                </View>
+              ))}
+            </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     </View>
